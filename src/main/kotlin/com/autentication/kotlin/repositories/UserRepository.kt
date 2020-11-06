0795522df9d360ecc4c2cf330d3c1312c302b396
Jorge Mina <minajorge@hotmail.com>
2020-11-06 11:17:02 -0500

    Security Services Implements

--- src/main/kotlin/com/autentication/kotlin/repositories/UserRepository.kt
+++ src/main/kotlin/com/autentication/kotlin/repositories/UserRepository.kt
@@ -3,8 +3,14 @@ package com.autentication.kotlin.repositories
 import com.autentication.kotlin.models.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
-interface UserRepository: JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long> {
 
+    fun findByUsername(username: String): Optional<User>
+
+    fun existsByUsername(username: String): Boolean
+
+    fun existsByEmail(email: String): Boolean
 }
---
+++ src/main/kotlin/com/autentication/kotlin/security/services/UserDetailsServiceImpl.kt
@@ -0,0 +1,25 @@
+package com.autentication.kotlin.security.services
+
+import com.autentication.kotlin.models.User
+import com.autentication.kotlin.repositories.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserDetailsServiceImpl :UserDetailsService{
+
+    @Autowired
+    val repository: UserRepository? = null
+
+    @Transactional
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): UserDetails? {
+        val user: User = repository!!.findByUsername(username)
+                .orElseThrow { UsernameNotFoundException("No se ha encontrado el usuario: $username") }
+        return UserDetailsImpl.build(user)
+    }
+}
